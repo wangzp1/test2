9a7a8959024109b1bdd48c7ebf1a105d3d6945b8
wangzp <1759699454@qq.com>
2020-06-01 14:44:34 +0800

    add hook

---
+++ hook/index.php
@@ -0,0 +1,19 @@
+<?php
+ 
+	error_reporting(1);
+ 
+	$target = '/data1/www/htdocs/githubtest.com/test1/'; // 生产环境web目录
+	$token = '123456';
+	$wwwUser = 'www';
+ 
+	$json = json_decode(file_get_contents('php://input'), true);
+ 
+	if (empty($json['token']) || $json['token'] !== $token) {
+	   // exit('error request');
+	   echo "error";
+	}
+	
+	$cmd = "cd $target && git pull";
+	
+	shell_exec($cmd);
+?>
